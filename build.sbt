982597fb07c42388631d3fdcba803ab362fa4067
thom <tom@tksfz.org>
2018-09-02 15:18:06 -0700

    Add basic server and service
    Copied from https://github.com/hhandoko/http4s-graal

--- build.sbt
+++ build.sbt
@@ -1,3 +1,11 @@
 name := "wg-config-http"
 
 scalaVersion := "2.12.6"
+
+val http4sVersion  = "0.18.9"
+
+libraryDependencies ++= Seq(
+  "org.http4s"     %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s"     %% "http4s-circe"        % http4sVersion,
+  "org.http4s"     %% "http4s-dsl"          % http4sVersion,
+)
---
+++ src/main/scala/org/tksfz/wgconfig/Server.scala
@@ -0,0 +1,30 @@
+package org.tksfz.wgconfig
+
+import scala.concurrent.ExecutionContext
+
+import cats.effect.{Effect, IO}
+import fs2.StreamApp
+import org.http4s.HttpService
+import org.http4s.server.blaze.BlazeBuilder
+
+object Server extends StreamApp[IO] {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = ServerStream.stream[IO]
+}
+
+object ServerStream {
+
+  def helloWorldService[F[_]: Effect]: HttpService[F] = new Service[F].service
+
+  def stream[F[_]: Effect](implicit ec: ExecutionContext): fs2.Stream[F, StreamApp.ExitCode] = {
+    val host = Option(System.getenv("APP_HOST")).getOrElse("0.0.0.0")
+    val port = Option(System.getenv("APP_PORT")).map(_.toInt).getOrElse(8080)
+
+    BlazeBuilder[F]
+      .bindHttp(port, host)
+      .mountService(helloWorldService, "/")
+      .serve
+  }
+
+}
