034e7c640820c024e92730277b2e83c9ad613e9a
thom <tom@tksfz.org>
2018-09-03 15:51:23 -0700

    Pass config from main to Service

--- src/main/scala/org/tksfz/wgconfig/Server.scala
+++ src/main/scala/org/tksfz/wgconfig/Server.scala
@@ -10,20 +10,24 @@ import org.http4s.server.blaze.BlazeBuilder
 object Server extends StreamApp[IO] {
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = ServerStream.stream[IO]
+  def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = {
+    val config =
+      ServerConfig("wg0", "endpoint", "pubkey", null, null)
+    ServerStream.stream[IO](config)
+  }
 }
 
 object ServerStream {
 
-  def helloWorldService[F[_]: Effect]: HttpService[F] = new Service[F].service
+  def helloWorldService[F[_]: Effect](config: ServerConfig): HttpService[F] = new Service[F](config).service
 
-  def stream[F[_]: Effect](implicit ec: ExecutionContext): fs2.Stream[F, StreamApp.ExitCode] = {
+  def stream[F[_]: Effect](config: ServerConfig)(implicit ec: ExecutionContext): fs2.Stream[F, StreamApp.ExitCode] = {
     val host = Option(System.getenv("APP_HOST")).getOrElse("0.0.0.0")
     val port = Option(System.getenv("APP_PORT")).map(_.toInt).getOrElse(8080)
 
     BlazeBuilder[F]
       .bindHttp(port, host)
-      .mountService(helloWorldService, "/")
+      .mountService(helloWorldService(config), "/")
       .serve
   }
 
--- src/main/scala/org/tksfz/wgconfig/Service.scala
+++ src/main/scala/org/tksfz/wgconfig/Service.scala
@@ -5,13 +5,22 @@ import io.circe.Json
 import org.http4s.HttpService
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.headers.`Content-Disposition`
 
-class Service[F[_]: Effect] extends Http4sDsl[F] {
+class Service[F[_]: Effect](config: ServerConfig) extends Http4sDsl[F] {
+  val wg = new Wg(config)
 
   val service: HttpService[F] = {
     HttpService[F] {
       case GET -> Root / "hello" / name =>
         Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+      case POST -> Root / "clients" / "new" =>
+        wg.findAvailableClientIp().map { clientIp =>
+          val clientConfig = wg.newClientConfig(clientIp.getAddress)
+          Ok(clientConfig).putHeaders(`Content-Disposition`("attachment", Map("filename" -> "wireguard.conf")))
+        }.getOrElse {
+          NotFound()
+        }
     }
   }
 }
