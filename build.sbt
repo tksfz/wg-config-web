72fab638868a1f866e10eb98c8be340fe476a46a
thom <tom@tksfz.org>
2018-09-03 15:48:19 -0700

    Config generation

--- build.sbt
+++ build.sbt
@@ -2,6 +2,11 @@ name := "wg-config-http"
 
 scalaVersion := "2.12.6"
 
+libraryDependencies ++= Seq(
+  "com.github.pathikrit" %% "better-files" % "3.4.0",
+  "com.github.veqryn" % "cidr-ip-trie" % "1.0.1"
+)
+
 val http4sVersion  = "0.18.9"
 
 libraryDependencies ++= Seq(
--- src/main/scala/org/tksfz/wgconfig/Wg.scala
+++ src/main/scala/org/tksfz/wgconfig/Wg.scala
@@ -2,15 +2,48 @@ package org.tksfz.wgconfig
 
 import java.io._
 
-case class IpRange(ipAddress: String, netmask: Int) {
-  override def toString = s"$ipAddress/$netmask"
+import better.files.File
+import com.github.veqryn.net.{Cidr4, Ip4}
+
+import scala.sys.process._
+
+case class IpRange(ipAddress: String, bits: Int) {
+  def toCidr = s"$ipAddress/$bits"
 }
+
+object IpRange {
+  def fromString(s: String) = {
+    val Cidr = """([\d\.]+)/(\d+)""".r
+    val Cidr(ipAddress, netmaskStr) = s
+    IpRange(ipAddress, netmaskStr.toInt)
+  }
+}
+
 case class ServerConfig(interface: String, serverEndpoint: String, serverPublicKey: String,
                         clientIpRange: IpRange, serverLanRange: IpRange)
 
-class Wg {
+class Wg(config: ServerConfig) {
 
-  def newClientConfig(config: ServerConfig, clientIp: String) = {
+  def findAvailableClientIp() = {
+    val showConf = s"wg showconf ${config.interface}".lineStream_!
+    val used = showConf
+      .filter(_.startsWith("AllowedIPs = "))
+      .flatMap { line =>
+        val cidrStr = line.split('=')(1).trim
+        val cidr = new Cidr4(cidrStr)
+        if (cidr.getAddressCount(false) == 1) {
+          Some(cidr.getLowIp(false))
+        } else {
+          None
+        }
+      }
+      .toSet
+    // TODO: lazy val
+    val cidr4 = new Cidr4(config.clientIpRange.toCidr)
+    cidr4.getAllIps(false).find(!used.contains(_))
+  }
+
+  def newClientConfig(clientIp: String) = {
 
     val (clientPublicKey, clientPrivateKey) = generatePrivateAndPublicKeys()
 
@@ -21,21 +54,24 @@ class Wg {
         |AllowedIPs = ${config.clientIpRange}
         |""".stripMargin
 
+    val f = File.newTemporaryFile("wgconfig-", ".conf", Some(File(System.getProperty("java.io.tmpdir"))))
+    f.overwrite(addServerConfig)
+
+    s"wg addconf ${config.interface} ${f.pathAsString}".!
+
     val clientConfig =
       s"""[Interface]
         |PrivateKey = $clientPrivateKey
         |ListenPort = 5555
-        |Address = $clientIp/${config.serverLanRange.netmask}
+        |Address = $clientIp/${config.serverLanRange.bits}
         |
         |[Peer]
         |PublicKey = ${config.serverPublicKey}
         |Endpoint = ${config.serverEndpoint}
-        |AllowedIPs = ${config.serverLanRange}
+        |AllowedIPs = ${config.serverLanRange.toCidr}
       """.stripMargin
 
-
-
-
+    clientConfig
   }
 
   def generatePrivateAndPublicKeys() = {
